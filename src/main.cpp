a28aed4c89431978c3c7636876a7a1bacb14e4ca
Alexander Almaleh <sashoalm@gmail.com>
2016-07-09 14:59:38 +0300

    Redundant I think.

--- src/main.cpp
+++ src/main.cpp
@@ -29,8 +29,6 @@
 #include "Settings.h"
 
 int main(int argc, char* argv[]) {
-  Q_INIT_RESOURCE(application);
-
   QApplication app(argc, argv);
   app.setOrganizationName("qt-box-editor");
   app.setApplicationName("QT Box Editor");
